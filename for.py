550d9a8c6546e347258a839a56105db6f69119d0
root <w8833531@hotmail.com>
2017-05-31 13:56:55 +0800

    delete awesome-python-webapp/backup/backup.txt

--- for.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding utf-8 -*-
-names = ['Michael', 'Bob', 'Tracy']
-for name in names:
-    print name
-sum=0
-for x in range(100):
-    sum = sum + x
-print sum
--- hello.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-print u'中文测试正常'
-print u'你好，%s' % u'世界'
-print u'你好，%s %02d世纪 欢迎你' % (u'吴鹰', 21)
-print u'你好冒险者，我是穆，请问你是'
-name=raw_input(u'请输入姓名:'.encode('utf-8')).decode('utf-8')
-print u'%s，你好，真是一个不错的名字，欢迎来到%s'%(name,u"法兰城")
